343ba600d76f3b53c2bb9f622a0dde44e4104b36
Andre Luiz <andre.bluiz94@gmail.com>
2019-09-15 11:20:20 -0300

    Primeiro commit e lá vamos nós
    Signed-off-by: PolianaLima <pollyannati@hotmail.com>

---
+++ Aula11/ex007.js
@@ -0,0 +1,6 @@
+var vel=78.2
+console.log(`A velocidade do seu carro é ${vel} KM/h`)
+if(vel>60){//condição simples
+    console.log('Voce ultrapssou a velocidade permitida. MULTADO!')
+}
+console.log(`Dirija sempre com cinto de segurnaça!`)
